7126aa75eb6b285f7cbffe46eaca4d895b66277a
Bruce Eddy <bruce@kandagawa.(none)>
2011-05-20 08:06:12 +0100

    remove libraries

--- Group3/graft_2.8.0-0.4.2-project/project/plugins/src_managed/spde-sbt-plugin-0.4.2/AndroidProject.scala
+++
@@ -1,8 +0,0 @@
-package spde
-
-import sbt._
-
-trait SpdeAndroidProject extends SpdeProject {
-  override def spde_artifact = super.spde_artifact intransitive()
-  val spde_core = "org.processing" % "core-android" % "1.1"
-}
